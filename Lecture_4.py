87d60ea1e1c3353de3d0516aefff85952ec6d0ce
Justin Chang <changjm2@students.cs.ubc.ca>
2024-06-25 12:36:11 +0800

    start of Lec 4

---
+++ Lecture_4.py
@@ -0,0 +1,8 @@
+# Lecture on Linear Regression
+# we shall use mini-batch fashion to train
+
+import torch
+
+# x and y are 3*1 size matrix
+x_data = torch.Tensor([[1.0],[2.0],[3.0]])
+y_data = torch.Tensor([[2.0],[4.0],[6.0]])
